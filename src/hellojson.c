9b176824f18af0bed36c4bfef0dfff43d7545aec
Art Navsegda <artnavsegda@gmail.com>
2019-04-26 12:56:57 +0300

    hellojson

--- src/hellojson.c
+++ src/hellojson.c
@@ -6,19 +6,24 @@ char buffer[10000];
 
 int main()
 {
+	cJSON * priceline = NULL;
 	FILE * jsonfile = fopen("./json.txt", "r");
 	if (jsonfile == NULL)
 		exit(1);
 	if (fread(buffer,1,10000,jsonfile) == 0)
 		exit(1);
 	
-	cJSON *json = cJSON_Parse(buffer);
-	if (monitor_json == NULL)
+	cJSON * pricelines = cJSON_Parse(buffer);
+	if (pricelines == NULL)
 	{
 		const char *error_ptr = cJSON_GetErrorPtr();
 		if (error_ptr != NULL)
 			fprintf(stderr, "Error before: %s\n", error_ptr);
 		exit(1);
+	}
+
+	cJSON_ArrayForEach(priceline, pricelines)
+	{
 	}
 	
 	return 0;
