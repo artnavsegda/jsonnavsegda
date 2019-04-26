7472d63d7935d694c624239b8b49ca6a08a52ca5
Art Navsegda <artnavsegda@gmail.com>
2019-04-26 12:42:16 +0300

    json

--- src/hellojson.c
+++ src/hellojson.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <cjson/cJSON.h>
+
+char buffer[10000];
 
 int main()
 {
+	FILE * jsonfile = fopen("./json.txt", "r");
+	if (jsonfile == NULL)
+		exit(1);
+	if (fread(buffer,1,10000,jsonfile) == 0)
+		exit(1);
+	
+	cJSON *json = cJSON_Parse(buffer);
+	if (monitor_json == NULL)
+	{
+		const char *error_ptr = cJSON_GetErrorPtr();
+		if (error_ptr != NULL)
+			fprintf(stderr, "Error before: %s\n", error_ptr);
+		exit(1);
+	}
+	
 	return 0;
 }
+
